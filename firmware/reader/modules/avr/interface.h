7fcbfa2b12e07e28db8060b1f8e21da7d09ed121
Oleksandr Kolodkin <oleksandr.kolodkin@ukr.net>
2025-07-21 08:41:27 +0300

    removed avr module

--- firmware/reader/modules/avr/interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef AVR_UTILITY_H
-#define AVR_UTILITY_H
-
-#define AVR_PORT_CONFIG(name, val) {uint8_t v = val; DDR##name = v; PORT##name = ~v;} //output(1)-low input(0)-pullup
-#define AVR_PIN_HIGH(port, index) PORT##port |= 1<<P##port##index;
-#define AVR_PIN_LOW(port, index) PORT##port &= (~(1<<P##port##index));
-
-#endif
