6d7265f0380d3fa412ad649306a1ffc489ea194c
Oleksandr Kolodkin <oleksandr.kolodkin@ukr.net>
2025-09-01 09:58:34 +0300

    make native test crossplatform

--- tests/test_format_runner.cpp
+++ tests/test_format_runner.cpp
@@ -15,40 +15,64 @@
 //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <gtest/gtest.h>
+
+#ifdef _WIN32
 #include <process.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <cstring>
+#endif
 
 #include <chrono>
 #include <iostream>
 #include <string>
 #include <thread>
 
-#pragma comment(lib, "ws2_32.lib")
-
 class QEMUTestRunner {
    private:
+#ifdef _WIN32
     SOCKET sock;
     HANDLE qemu_process;
+#else
+    int sock;
+    pid_t qemu_process;
+#endif
     bool connected;
 
    public:
-    QEMUTestRunner() : sock(INVALID_SOCKET), qemu_process(nullptr), connected(false) {
+    QEMUTestRunner() : sock(-1), qemu_process(0), connected(false) {
+#ifdef _WIN32
         WSADATA wsaData;
         WSAStartup(MAKEWORD(2, 2), &wsaData);
+        sock = INVALID_SOCKET;
+        qemu_process = nullptr;
+#endif
     }
 
     ~QEMUTestRunner() {
         cleanup();
+#ifdef _WIN32
         WSACleanup();
+#endif
     }
 
     bool startQEMU() {
         // Start QEMU with the AVR test
         std::string cmd =
-            "qemu-system-avr -machine arduino-uno -bios ../build/tests/test_format_avr.elf "
+            "qemu-system-avr -machine arduino-uno -bios ../build/tests-avr/test_format_avr.elf "
             "-nographic -serial telnet:localhost:1234,server,nowait";
 
+#ifdef _WIN32
         STARTUPINFOA si = {0};
         si.cb = sizeof(si);
         PROCESS_INFORMATION pi = {0};
@@ -61,6 +85,18 @@ class QEMUTestRunner {
 
         qemu_process = pi.hProcess;
         CloseHandle(pi.hThread);
+#else
+        // Linux implementation using fork/exec
+        qemu_process = fork();
+        if (qemu_process == 0) {
+            // Child process - exec QEMU
+            execl("/bin/sh", "sh", "-c", cmd.c_str(), (char*)nullptr);
+            exit(1);  // If exec fails
+        } else if (qemu_process < 0) {
+            std::cerr << "Failed to fork QEMU process" << std::endl;
+            return false;
+        }
+#endif
 
         // Wait for QEMU to start
         std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -70,14 +106,24 @@ class QEMUTestRunner {
 
     bool connectToQEMU() {
         sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+#ifdef _WIN32
         if (sock == INVALID_SOCKET) {
             return false;
         }
+#else
+        if (sock < 0) {
+            return false;
+        }
+#endif
 
         sockaddr_in addr;
         addr.sin_family = AF_INET;
         addr.sin_port = htons(1234);
+#ifdef _WIN32
         inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+#else
+        inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+#endif
 
         // Try to connect with retries
         for (int i = 0; i < 10; i++) {
@@ -110,8 +156,13 @@ class QEMUTestRunner {
         if (!connected) return "";
 
         // Set socket to non-blocking
+#ifdef _WIN32
         u_long mode = 1;
         ioctlsocket(sock, FIONBIO, &mode);
+#else
+        int flags = fcntl(sock, F_GETFL, 0);
+        fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+#endif
 
         std::string response;
         auto start_time = std::chrono::steady_clock::now();
@@ -138,13 +189,19 @@ class QEMUTestRunner {
         }
 
         // Set socket back to blocking
-        mode = 0;
+#ifdef _WIN32
+        u_long mode = 0;
         ioctlsocket(sock, FIONBIO, &mode);
+#else
+        int flags = fcntl(sock, F_GETFL, 0);
+        fcntl(sock, F_SETFL, flags & ~O_NONBLOCK);
+#endif
 
         return response;
     }
 
     void cleanup() {
+#ifdef _WIN32
         if (connected && sock != INVALID_SOCKET) {
             closesocket(sock);
             connected = false;
@@ -155,6 +212,19 @@ class QEMUTestRunner {
             CloseHandle(qemu_process);
             qemu_process = nullptr;
         }
+#else
+        if (connected && sock >= 0) {
+            close(sock);
+            connected = false;
+            sock = -1;
+        }
+
+        if (qemu_process > 0) {
+            kill(qemu_process, SIGTERM);
+            waitpid(qemu_process, nullptr, 0);
+            qemu_process = 0;
+        }
+#endif
     }
 };
 
